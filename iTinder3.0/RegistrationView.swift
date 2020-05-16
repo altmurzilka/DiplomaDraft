0a63cbb1ed12983683bc3c09c26ecf2d3e6910f9
altmurzilka <altmurzilka@gmail.com>
2020-05-16 06:56:51 +0600

    RegView massive update

--- iTinder3.0/RegistrationView.swift
+++ iTinder3.0/RegistrationView.swift
@@ -49,88 +49,112 @@ struct Login: View {
     @State private var pass = ""
     @State private var visible = false
     @Binding var show : Bool
+    @State var alert = false
+    @State var error = ""
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            GeometryReader {_ in
-                VStack {
-                    Image(systemName: "person.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                        .foregroundColor(Color("Color"))
-                    Text("Login into your account")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(self.color)
-                        .padding(.top, 35)
-                    
-                    TextField("Email", text: self.$email)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : self.color,lineWidth: 2))
-                        .padding(.top, 25)
-                    
-                    HStack(spacing: 15) {
-                        VStack {
-                            if self.visible {
-                                TextField("Password", text: self.$pass)
-                            } else{
-                                SecureField("Password", text: self.$pass)
+        ZStack {
+            ZStack(alignment: .topTrailing) {
+                GeometryReader {_ in
+                    VStack {
+                        Image(systemName: "person.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .foregroundColor(Color("Color"))
+                        Text("Login into your account")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(self.color)
+                            .padding(.top, 35)
+                        
+                        TextField("Email", text: self.$email)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color") : self.color,lineWidth: 2))
+                            .padding(.top, 25)
+                        
+                        HStack(spacing: 15) {
+                            VStack {
+                                if self.visible {
+                                    TextField("Password", text: self.$pass)
+                                } else{
+                                    SecureField("Password", text: self.$pass)
+                                }
+                            }
+                            
+                            Button(action: {
+                                
+                                self.visible.toggle()
+                                
+                            }) {
+                                Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(self.color)
                             }
                         }
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
+                        .padding(.top, 25)
                         
-                        Button(action: {
+                        HStack {
+                            Spacer()
                             
-                            self.visible.toggle()
+                            Button(action: {
+                                
+                                
+                            }) {
+                                Text("Forgot password")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("Color"))
+                            }
+                        }
+                        .padding(.top, 20)
+                        
+                        Button( action: {
+                            
+                            self.verify()
                             
                         }) {
-                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
-                                .foregroundColor(self.color)
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .padding(.vertical)
+                                .frame(width: UIScreen.main.bounds.width - 50)
                         }
+                        .background(Color("Color"))
+                        .cornerRadius(10)
+                        .padding(.top, 25)
                     }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.pass != "" ? Color("Color") : self.color,lineWidth: 2))
-                    .padding(.top, 25)
-                    
-                    HStack {
-                        Spacer()
-                        
-                        Button(action: {
-                            
-                            
-                        }) {
-                            Text("Forgot password")
-                                .fontWeight(.bold)
-                                .foregroundColor(Color("Color"))
-                        }
-                    }
-                    .padding(.top, 20)
-                    
-                    Button( action: {
-                        
-                    }) {
-                        Text("Log In")
-                            .foregroundColor(.white)
-                            .padding(.vertical)
-                            .frame(width: UIScreen.main.bounds.width - 50)
-                    }
-                    .background(Color("Color"))
-                    .cornerRadius(10)
-                    .padding(.top, 25)
+                    .padding(.horizontal, 25)
                 }
-                .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.show.toggle()
+                    
+                }) {
+                    Text("Register")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                }
+                .padding()
             }
             
-            Button(action: {
+            if self.alert{
                 
-                self.show.toggle()
-                
-            }) {
-                Text("Register")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("Color"))
+                ErrorView(alert: self.$alert, error: self.$error)
             }
-            .padding()
+        }
+    }
+    
+    func verify(){
+        
+        if self.email != "" && self.pass != ""{
+            
+            
+        }
+        else{
+            
+            self.error = "Please fill all the contents properly"
+            self.alert.toggle()
         }
     }
 }
@@ -242,6 +266,8 @@ struct SignUp: View {
 struct ErrorView: View {
     
     @State private var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error : String
     
     var body: some View {
         
@@ -254,10 +280,36 @@ struct ErrorView: View {
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(self.color)
+                    
+                    Spacer()
                 }
+                .padding(.horizontal, 25)
                 
+                Text(self.error)
+                    .foregroundColor(self.color)
+                    .padding(.top)
+                    .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.alert.toggle()
+                    
+                }) {
+                    Text("Cancel")
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
             }
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width - 70)
+            .background(Color.white)
+            .cornerRadius(15)
         }
+        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
     }
 }
 
